9c5aca07d67bce89ff36300f642c08781792d804
Jean-AIME-svg <jeanaimei56@gmail.com>
2024-06-08 10:21:05 -0700

    first commit

---
+++ src/assets/footer.js
@@ -0,0 +1,6 @@
+function footer(){
+    return(
+        <h1>hello</h1>
+    )
+}
+export default footer;
---
+++ src/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import header from './header';
+
+function App() {
+  return (
+    <div>
+      <header />
+    </div>
+  );
+}
+
+export default App;
